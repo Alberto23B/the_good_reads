b93a61b4e5b1892e88336897323b82c48864621d
Alberto Biolchi <169494833+Alberto23B@users.noreply.github.com>
2024-10-01 22:47:46 +0200

    fix quote to change only on refresh

--- src/components/Quote.js
+++ src/components/Quote.js
@@ -1,8 +1,12 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 import { getRandomQuote } from "../quoteData";
 
 function Quote() {
-    const quote = getRandomQuote();
+    const [quote, setQuote] = useState("");
+
+    useEffect(() => {
+        setQuote(getRandomQuote())
+    }, [])
     
     return (
         <p className="h-12 max-w-2xl mx-auto text-xs italic sm:text-base">{quote}</p>
