bb24fc38e576e4e01008554fcd7db346eab89a75
Alberto Biolchi <169494833+Alberto23B@users.noreply.github.com>
2024-12-11 11:20:25 +0100

    Make the header a link to homepage

--- src/presentational/Header.js
+++ src/presentational/Header.js
@@ -7,13 +7,15 @@ export default function Header() {
   return (
     <header className="flex w-5/6 mx-auto mt-4 text-center">
       <div className="w-full">
-        <h1
-          className={`mb-8 font-semibold text-3xl ${
-            isInputSelected ? " md:animate-resizeTitle" : ""
-          } sm:text-8xl font-lcd`}
-        >
-          The Good Reads
-        </h1>
+        <a href="/">
+          <h1
+            className={`mb-8 font-semibold text-3xl ${
+              isInputSelected ? " md:animate-resizeTitle" : ""
+            } sm:text-8xl font-lcd`}
+          >
+            The Good Reads
+          </h1>
+        </a>
       </div>
     </header>
   );
